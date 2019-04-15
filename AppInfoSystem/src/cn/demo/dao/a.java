f3ff25875f80814dae54c3684990316c94aa7702
gin <754049241@qq.com>
2019-04-15 17:18:22 +0800

    空包加了类

---
+++ AppInfoSystem/src/cn/demo/dao/a.java
@@ -0,0 +1,5 @@
+package cn.demo.dao;
+
+public class a {
+
+}
---
+++ AppInfoSystem/src/cn/demo/pojo/Version.java
@@ -0,0 +1,98 @@
+package cn.demo.pojo;
+
+import java.util.Date;
+
+public class Version {
+	private int id;//编号
+	private int appId;//信息
+	private String versionNo;//版本号
+	private String versionInfo;//版本介绍
+	private	int publishStatus;//发布状态
+	private	String downloadLink;//下载链接
+	private	double versionSize;//版本大小
+	private	int createdBy;//创建者
+	private	Date creationDate;//创建时间
+	private	int modifyBy;//修改者
+	private Date modifyDate;//修改时间
+	private	String apkLocPath;//apk文件的服务器存储路径
+	private String apkFileName;//上传的apk文件名称
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAppId() {
+		return appId;
+	}
+	public void setAppId(int appId) {
+		this.appId = appId;
+	}
+	public String getVersionNo() {
+		return versionNo;
+	}
+	public void setVersionNo(String versionNo) {
+		this.versionNo = versionNo;
+	}
+	public String getVersionInfo() {
+		return versionInfo;
+	}
+	public void setVersionInfo(String versionInfo) {
+		this.versionInfo = versionInfo;
+	}
+	public int getPublishStatus() {
+		return publishStatus;
+	}
+	public void setPublishStatus(int publishStatus) {
+		this.publishStatus = publishStatus;
+	}
+	public String getDownloadLink() {
+		return downloadLink;
+	}
+	public void setDownloadLink(String downloadLink) {
+		this.downloadLink = downloadLink;
+	}
+	public double getVersionSize() {
+		return versionSize;
+	}
+	public void setVersionSize(double versionSize) {
+		this.versionSize = versionSize;
+	}
+	public int getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public int getModifyBy() {
+		return modifyBy;
+	}
+	public void setModifyBy(int modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public String getApkLocPath() {
+		return apkLocPath;
+	}
+	public void setApkLocPath(String apkLocPath) {
+		this.apkLocPath = apkLocPath;
+	}
+	public String getApkFileName() {
+		return apkFileName;
+	}
+	public void setApkFileName(String apkFileName) {
+		this.apkFileName = apkFileName;
+	}
+}
